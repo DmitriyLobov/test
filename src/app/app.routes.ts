3357f66001cdcecf733eebab7c3d98a88a6915f2
Tokyo <dimkalobov95@gmail.com>
2017-06-10 12:20:10 +0300

    v 1.0a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import {Routes} from "@angular/router";
-/**
- * Created by tokyo on 6/10/17.
- */
 
 export const APP_ROUTES: Routes = [
   {path: '', loadChildren:'./calculation/calculation.module#CalculationModule'}
--- src/app/calculation/calculation.routes.ts
+++ src/app/calculation/calculation.routes.ts
@@ -1,8 +1,5 @@
 import {Routes} from "@angular/router";
 import {CalculationMainPageComponent} from "./pages/calculation-main-page/calculation-main-page.component";
-/**
- * Created by tokyo on 6/10/17.
- */
 
 export const CALCULATION_ROUTES: Routes = [
   {path: '', component: CalculationMainPageComponent}
